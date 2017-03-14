fdf98d9b94d334c7059ecc74ce9c82c5ba915154
Tarang Hirani <tarang9211@gmail.com>
2017-03-14 17:14:18 +0530

    modified webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+var webpack = require('webpack');
 module.exports = {
   entry: [
     './src/index.js'
@@ -22,5 +24,17 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     contentBase: './'
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: true
+      }
+    })
+  ]
 };
